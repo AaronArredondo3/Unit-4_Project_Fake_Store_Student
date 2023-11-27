edd682d04fb32e56e64713ad5d45b88436ca5e26
AaronArredondo3 <Aaronarredondo63093@gmail.com>
2023-11-27 01:04:25 -0600

    Finished ticket 3.

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,46 @@
 //! GLobal Variables
-const apiUrl = 'https://fakestoreapi.com/'
+const apiUrl =('https://fakestoreapi.com/products')
+// const endPoint= 
 
 const category1= document.getElementsByClassName('Cart');
-const category2= document.getElementsByClassName('Electronics');
-const category3= document.getElementsByClassName('Jewelery');
-const category4= document.getElementsByClassName('Mens Clothing');
-const category5= document.getElementsByClassName('Womens Clothing');
+const category2= document.getElementById('Electronics');
+const category3= document.getElementById('Jewelery');
+const category4= document.getElementById('Mens Clothing');
+const category5= document.getElementById('Womens Clothing');
 const displayDiv = document.getElementById('display');
 
 //!Async Function
 
-const fakeStore = async(endpoint)=>{
-    try {
-        let response = await fetch(apiUrl);
-        // if (!response.ok) {
-        //     throw new Error(`HTTP error! Status: ${response.status}`);
-        // }
-        let data = await response.json();
-        console.log(data); // Log data to console for testing
-        // You can manipulate the data and update the display here
-    } catch (error) {
-        console.error('Error fetching data:', error);
+const fakeStore = async(endpoint) => {
+    try{
+        const res= await fetch (`${apiUrl}/${endpoint}`);
+        const data = await res.json();
+        console.log(data[0]);
+    }  catch (err) {
+        console.error('err')
     }
-}
 
-fakeStore(endpoint);
+};
+
+//! Eventlistener for each category
+
+category2.addEventListener('click', e => {
+    fakeStore('Electronics');
+});
+
+category3.addEventListener('click', e => {
+    fakeStore('Jewekery');
+});
+
+category4.addEventListener('click', e => {
+    fakeStore('Mens Clothing');
+});
+
+category5.addEventListener('click', e => {
+    fakeStore('Womens CLothing');
+});
+
+//! Window onload event
+window.onload = () => {
+    fakeStore('?sort=asc');
+};
