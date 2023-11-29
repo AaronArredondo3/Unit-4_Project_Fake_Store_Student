8d8e081c25e44b2122ae5ff448cd4c67ee797fb3
AaronArredondo3 <Aaronarredondo63093@gmail.com>
2023-11-29 01:16:52 -0600

    Updated code for ticket4.

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ const category1= document.getElementById('Electronics');
 const category2= document.getElementById('Jewelery');
 const category3= document.getElementById('Mens Clothing');
 const category4= document.getElementById('Womens Clothing');
-const displayDiv = document.getElementById('display');
+const displayDiv = document.querySelector('#display');
 
 //!Async Function
 
@@ -27,10 +27,10 @@ const fakeStore = async(endpoint) => {
 
 //! Function to display the cards
 const displayCards = function (data) {
-        const displayDiv = document.getElementById('display');
+        // const displayDiv = document.querySelector('#display');
         data.forEach (product => {
         
-                //* Create Elements
+            //* Create Elements
             let cardDiv = document.createElement('div');
             let image = document.createElement('img');
             let cardBody = document.createElement('div');
@@ -38,56 +38,53 @@ const displayCards = function (data) {
             let description = document.createElement('p')
             let price = document.createElement('p');
             let accordionDiv = document.createElement('div');
-            let accordionButton = document.createElement('button');
             let accordionContent = document.createElement('div')
             let addToCartButton = document.createElement('button');
         
-                //* Set Attributes
+            //* Set Attributes
             cardDiv.classList.add('card', 'mb-3');
-    image.src = product.image;
-    image.alt = product.title;
-    image.classList.add('card-img-top');
-    cardBody.classList.add('card-body');
-    title.textContent = product.title;
-    title.classList.add('card-title');
-    description.textContent = product.description;
-    description.classList.add('card-text');
-    price.textContent = product.description;
-    price.classList.add('card-text');
-    accordionDiv.classList.add('accordion');
-    accordionButton.textContent = 'Show Details';
-    accordionButton.classList.add('btn', 'btn-link');
-    accordionContent.classList.add('accordion-content');
-    addToCartButton.textContent = 'Add to Cart';
-    addToCartButton.classList.add('btn', 'btn-primary', 'mt-2');
-
-    //*Attach Elements
-    accordionDiv.appendChild(accordionButton);
-        accordionDiv.appendChild(accordionContent);
-
+            image.src = product.image;
+            image.alt = product.title;
+            image.classList.add('card-img-top');
+            cardBody.classList.add('card-body');
+            title.textContent = product.title;
+            title.classList.add('card-title');
+            description.textContent = product.description;
+            description.classList.add('card-text');
+            price.textContent = `$${product.price.toFixed(2)}`;
+            price.classList.add('card-text');
+            accordionDiv.classList.add('accordion');
+            accordionContent.classList.add('accordion-content');
+            addToCartButton.textContent = 'Add to Cart';
+            addToCartButton.classList.add('btn', 'btn-primary', 'mt-2');
+            
+            addToCartButton.onclick = () => {
+                accordionButton.addEventListener('click', () => {
+                    accordionContent.classList.toggle('show');
+                });
+                const newItem = {
+                    id: product.id,
+                    title: product.title,
+                    cost: product.price.toFixed(2),
+                    quantity: 1,
+                };
+                submitToCart(newItem);
+            };
+            
+            //*Attach Elements
+            accordionDiv.appendChild(accordionContent);
+            
         cardBody.appendChild(title);
         cardBody.appendChild(description);
         cardBody.appendChild(price);
         cardBody.appendChild(accordionDiv);
-
+        
         cardDiv.appendChild(image);
         cardDiv.appendChild(cardBody);
         cardDiv.appendChild(addToCartButton);
         
         displayDiv.appendChild(cardDiv);
         
-        addToCartButton.onclick = () => {
-            accordionButton.addEventListener('click', () => {
-                accordionContent.classList.toggle('show');
-            });
-            const newItem = {
-                id: product.id,
-                title: product.title,
-                cost: product.price.toFixed(2),
-                quantity: 1,
-            };
-            submitToCart(newItem);
-        };
         
     });
 };
